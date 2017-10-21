2aa5acc9756bb55bdbd37b894f6caac977be0e09
Kajamuhaideen <32951423+Kajamuhaideen@users.noreply.github.com>
2017-10-21 15:23:52 +0530

    Add files via upload

---
+++ var&nms.py
@@ -0,0 +1,21 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      16ece026
+#
+# Created:     21/10/2017
+# Copyright:   (c) 16ece026 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+
+ counter=100
+ miles  =1000.0
+ name   ="John"
+print counter
+print miles
+print name
+if __name__ == '__main__':
+    main()
